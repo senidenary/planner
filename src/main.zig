dfdd7a3593f9ae549eac05cf340b004a522f2124
Harold Absalom <harold@absalom.com.au>
2025-09-02 09:11:23 +1000

    Initial commit

---
+++ src/main.zig
@@ -0,0 +1,38 @@
+const std = @import("std");
+const time = @import("time.zig");
+
+pub fn main() !void {
+    const t = time.DateTime.now();
+    // Prints to stderr, ignoring potential errors.
+    std.debug.print("All your {d} are belong to us.\n", .{t.years});
+    try bufferedPrint();
+}
+
+fn bufferedPrint() !void {
+    // Stdout is for the actual output of your application, for example if you
+    // are implementing gzip, then only the compressed bytes should be sent to
+    // stdout, not any debugging messages.
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    const stdout = &stdout_writer.interface;
+
+    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+
+    try stdout.flush(); // Don't forget to flush!
+}
+
+fn add(a: i32, b: i32) i32 {
+    return a + b;
+}
+
+test "basic add functionality" {
+    try std.testing.expect(add(3, 7) == 10);
+}
+
+test "simple test" {
+    const gpa = std.testing.allocator;
+    var list: std.ArrayList(i32) = .empty;
+    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
+    try list.append(gpa, 42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
